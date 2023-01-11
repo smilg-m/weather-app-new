58a7c1deeb1221c1c7fd31f33db10ed832d1056c
smilg-m <migluze@gmail.com>
2023-01-11 17:10:20 +0200

    JS added icon from API

--- src/app.js
+++ src/app.js
@@ -26,7 +26,13 @@ function formatTime(timestamp) {
   ];
 
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let day = date.getDate();
   let month = months[date.getMonth()];
   let weekday = days[date.getDay()];
@@ -43,6 +49,7 @@ function updateTemperature(response) {
   let currentWind = document.querySelector("#wind-speed");
   let currentVisibility = document.querySelector("#visibility");
   let timeElement = document.querySelector("#time");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
   cityElement.innerHTML = response.data.name;
@@ -54,11 +61,16 @@ function updateTemperature(response) {
   currentWind.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
   currentVisibility.innerHTML = `${response.data.visibility / 1000} km`;
   timeElement.innerHTML = formatTime(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", `${response.data.weather[0].main}`);
 }
 
 let apiKey = "b95f179627c8dd37f41e1be6e3250e19";
 let units = "metric";
-let city = "Vilnius";
+let city = "Berlin";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 console.log(apiUrl);
 
